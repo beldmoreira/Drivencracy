dcc483534c27a9db149bdd8d250a5eefa12f71ae
Isabela <isabela_cmoreira@hotmail.com>
2022-05-17 14:47:05 -0300

    Initial setup of the project

--- index.js
+++ index.js
@@ -9,6 +9,25 @@ app.use(cors());
 
 dotenv.config();
 
+app.post("/poll",(req,res) => {
+    const {title,expireAt} = req.body;
+
+    const getpollSchema = joi.object({
+        title: joi.string().required()
+    });
+    const {error} = getpollSchema.validate(req.body,{abortEarly:false});
+    if(error){
+        return res.status(422).send(error.details.map(detail => detail.message));   
+    }
+
+    try{
+       await.db 
+    }catch(error){
+
+    }
+   
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}.`);
