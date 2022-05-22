a3caed80be9b96698c88d272699a8a9936e7aa5c
Isabela <isabela_cmoreira@hotmail.com>
2022-05-22 17:49:48 -0300

    feat: finished the first two requests

--- controllers/pollController.js
+++ controllers/pollController.js
@@ -3,74 +3,39 @@ import pollSchema from "../schemas/pollSchema.js";
 
 
 export async function createPoll(req,res){
-    console.log("nwgoihgr")
-    const {title,expireAt} = req.body;
-
+    let poll = req.body;
+  
     const {error} = pollSchema.validate(req.body,{abortEarly:false});
     if(error){
         return res.status(422).send(error.details.map(detail => detail.message));   
     }
 
+    let date = poll.expireAt;
+    if(!date){
+        date = new Date(new Date().setDate(new Date().getDate() + 30));
+        poll.expireAt = date;
+    }
+    
     try{
-        console.log("gegihe")
-       const { mongoClient, db } = await connection();
-        console.log("antes")
-       await db.collection("polls").insertOne(req.body);
-       console.log("depois")
-       await mongoClient.close(); 
-       res.sendStatus(201);
-    } catch(error){
+        const { mongoClient, db } = await connection();
+        await db.collection("polls").insertOne(req.body);
+        await mongoClient.close(); 
+        res.sendStatus(201);
+        } catch(error){
         res.sendStatus(500);
     }
    
 };
-
-// try {
-//     const SALT = 10;
-//     const passwordHash = bcrypt.hashSync(req.body.password, SALT);
-    
-//     await db.collection("users").insertOne({
-//       name: req.body.name,
-//       email: req.body.email,
-//       password: passwordHash
-//     });
-
-//     return res.sendStatus(201); // created
-//   } catch (error) {
-//     console.log("Error creating new user.");
-//     console.log(error);
-//     return res.sendStatus(500);
-//   }
   
-  
-//   const poll = req.body;
-
-//   const validate = pollSchema.validate(poll);
-
-//   if(validate.error){
-//       res.sendStatus(422)
-//   }
-
-//   try{
-//       const newPool = await db.collection('polls').insertOne(poll);
-//           if(newPool){
-//               res.sendStatus(201)
-//               return;
-//           }else(
-//               res.sendStatus(401)      
-//           )
-//       }catch(error){
-//           res.sendStatus(500);
-//       }
-
-// }
-
-
 export async function getPoll (req,res) {
-    const{_id,title,expireAt}= req.body;
     try{
-        await db.collection("polls").findOne(req.body);
-        res.sendStatus(201);
+        const { mongoClient, db } = await connection();
+        const polls = await db.collection("polls").find({}).toArray();
+        await mongoClient.close(); 
+        if(polls){
+           return res.status(200).send(polls);     
+        }
+        
     } catch(error){
         res.sendStatus(500);
     }
