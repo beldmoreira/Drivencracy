329a87dc55efd2ab647b788af24b23d0bacece14
Isabela <isabela_cmoreira@hotmail.com>
2022-05-23 08:08:55 -0300

    Fixed the schema

--- schemas/pollSchema.js
+++ schemas/pollSchema.js
@@ -2,7 +2,7 @@ import joi from "joi";
 
 const pollSchema = joi.object({
     title: joi.string().required(),
-    expireAt: joi.string()
+    expireAt: joi.string().required()
 });
 
 export default pollSchema;
